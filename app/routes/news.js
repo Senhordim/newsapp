e22bc8096b908353ff395055c668473d90cb6ba8
Diego Collares <diego.collares@gmail.com>
2017-07-01 18:16:26 -0300

    created query to list news

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,5 +1,16 @@
+
+var database = require('../../config/database');
+
 module.exports = function(app) {
+
+  var connection = database();
+
   app.get('/news', function(req, res){
-    res.render("news/index");
+
+    connection.query('SELECT * FROM NEWS', function(error, result){
+      // res.send(result);
+      res.render("news/index", {news : result});
+    });
+
   });
-}
+};
