bfc967a2a056780d6aeaf9250cd498ee71282fe2
Diego Collares <diego.collares@gmail.com>
2017-07-01 18:14:58 -0300

    config connection mysql

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// Mysql
+
+var mysql = require('mysql');
+
+module.exports = function () {
+  return mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password: '',
+    database : 'newsapp'
+  });
+}
+
