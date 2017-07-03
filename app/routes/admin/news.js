9120dffd0f552ed53d155294b03fa8fd5ba2feb4
Diego Collares <diego.collares@gmail.com>
2017-07-03 20:12:41 -0300

    validation fieds

--- app/routes/admin/news.js
+++ app/routes/admin/news.js
@@ -1,11 +1,23 @@
 module.exports = function(application) {
 
   application.get('/admin', function(req, res){
-    res.render("admin/news/new");
+    res.render("admin/news/new", {validation : {}, news :{} });
   });
 
   application.post('/admin/news/save', function(req, res){
     var news = req.body;
+
+    req.assert('title', 'Título é obrigatório').notEmpty();
+    req.assert('resume', 'Resumo é obrigatório').notEmpty();
+    req.assert('resume', 'Resumo deve ter entre 10 e 100 caracteris ').len(10, 100);
+    req.assert('body', 'Notícia é obrigátorio').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      res.render("admin/news/new", {validation : errors, news : news});
+      return;
+    }
 
     var connection = application.config.database();
     var newsModel = new application.app.models.News(connection);
