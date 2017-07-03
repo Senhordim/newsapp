2528022eafeadd95145624f1b7e8fb1cc67c2d7d
Diego Collares <diego.collares@gmail.com>
2017-07-03 15:49:48 -0300

    created method save

--- app/models/newsModel.js
+++ app/models/newsModel.js
@@ -1,11 +1,15 @@
 module.exports = function(){
 
   this.index = function(connection, callback){
-    connection.query('select * from noticias', callback);
+    connection.query('select * from news', callback);
   }
 
   this.show = function(connection, callback){
-    connection.query('select * from noticias where id = 1', callback);
+    connection.query('select * from news where id = 1', callback);
+  }
+
+  this.save = function(news, connection, callback){
+    connection.query('insert into news set ? ', news, callback);
   }
 
   return this;
