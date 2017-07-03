952678a16e2455b8d2257db647336c5bc98d54c6
Diego Collares <diego.collares@gmail.com>
2017-07-03 15:50:19 -0300

    created route save

--- app/routes/adminNews.js
+++ app/routes/adminNews.js
@@ -1,5 +1,20 @@
-module.exports = function(app) {
-  app.get('/admin', function(req, res){
+module.exports = function(application) {
+
+  application.get('/admin', function(req, res){
     res.render("admin/news/new");
+  });
+
+  application.post('/admin/news/save', function(req, res){
+    var news = req.body;
+
+    var connection = application.config.database();
+    var newsModel = application.app.models.newsModel();
+
+    newsModel.save(news, connection, function(error, result){
+      // res.send(result);
+      // res.send(news)
+      res.redirect('/news');
+    });
+
   });
 }
