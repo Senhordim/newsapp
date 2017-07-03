2c5e2f1a6e01e1563b7280823ee2992e31e6dd0a
Diego Collares <diego.collares@gmail.com>
2017-07-03 00:55:49 -0300

    reove comment

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var app = require('./config/server');
 
-// Definindo rotas
-var routeHome = require('./app/routes/home')(app);
-var routeNews = require('./app/routes/news')(app);
-var routeAdminNews = require('./app/routes/adminNews')(app);
-
-// Listar servidor em uma determinada porta
 app.listen(3000, function(){
   console.log("Server is up with express")
 });
