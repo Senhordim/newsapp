09c14b01cd7135d8f1c67e5c79aea77b669c9cbc
Diego Collares <diego.collares@gmail.com>
2017-07-04 22:03:05 -0300

    set request to controller

--- app/routes/admin/news.js
+++ app/routes/admin/news.js
@@ -1,32 +1,9 @@
 module.exports = function(application) {
-
   application.get('/admin', function(req, res){
-    res.render("admin/news/new", {validation : {}, news :{} });
+    application.app.controllers.admin.news.new(application, req, res);
   });
 
   application.post('/admin/news/save', function(req, res){
-    var news = req.body;
-
-    req.assert('title', 'Título é obrigatório').notEmpty();
-    req.assert('resume', 'Resumo é obrigatório').notEmpty();
-    req.assert('resume', 'Resumo deve ter entre 10 e 100 caracteris ').len(10, 100);
-    req.assert('body', 'Notícia é obrigátorio').notEmpty();
-
-    var errors = req.validationErrors();
-
-    if (errors) {
-      res.render("admin/news/new", {validation : errors, news : news});
-      return;
-    }
-
-    var connection = application.config.database();
-    var newsModel = new application.app.models.News(connection);
-
-    newsModel.save(news, function(error, result){
-      // res.send(result);
-      // res.send(news)
-      res.redirect('/news');
-    });
-
+    application.app.controllers.admin.news.save(application, req, res);
   });
 }
--- app/routes/news.js
+++ app/routes/news.js
@@ -2,31 +2,12 @@ module.exports = function(application) {
 
   // Index
   application.get('/news', function(req, res){
-
-    var connection = application.config.database();
-    var newsModel = new application.app.models.News(connection);
-
-    newsModel.index(function(error, result){
-      // res.send(result);
-      res.render("news/index", {news : result});
-    });
-
+    application.app.controllers.news.index(application, req, res)
   });
 
   // Show
   application.get('/news/:id?', function(req, res){
-
-    var paramsUrl = req.params.id;
-    var connection = application.config.database();
-    var newsModel = new application.app.models.News(connection);
-
-    console.log(paramsUrl);
-
-    newsModel.show(paramsUrl, function(error, result){
-      // res.send(result);
-      res.render("news/show", {news : result});
-    });
-
+    application.app.controllers.news.show(application, req, res)
   });
 
 };
