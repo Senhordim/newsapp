ee71cd8786d3ae253cb147b1a0cca19b4f6271fb
Diego Collares <diego.collares@gmail.com>
2017-07-05 14:32:24 -0300

    las 5 news in home

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,11 @@
 module.exports.index = function(application, req, res){
-  res.render("home/index")
+
+  var connection = application.config.database();
+  var newsModel = new application.app.models.News(connection);
+
+  newsModel.getLastNesws(function(error, result){
+    console.log(result);
+    res.render("home/index", {news : result});
+  });
+
 }
